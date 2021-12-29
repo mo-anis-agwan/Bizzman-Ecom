9b223adf52a35d14b1bcfe2ed72210b6a3bbbf0e
Anis Agwan <anis.agwan@wsisrdev.net>
2021-12-30 02:07:37 +0530

    Initial Commit

---
+++ Shared/Bizzman_EcomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bizzman_EcomApp.swift
+//  Shared
+//
+//  Created by Anis Agwan on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Bizzman_EcomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
